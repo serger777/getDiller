95fe0cb76efaf5bf4f6dc4ba80f13140a11f192e
sergey.gerasimov <serger777@gmail.com>
2020-04-14 19:33:12 +0300

    Updates

---
+++ precache-manifest.1788948157d5a674963f0e1b25ca0523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada07682dec68f1becd7151554016756",
+    "url": "/getDiller/index.html"
+  },
+  {
+    "revision": "9928b348bcc440f422fd",
+    "url": "/getDiller/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "2fd1f3dc774611d69e39",
+    "url": "/getDiller/static/css/main.2c4c944b.chunk.css"
+  },
+  {
+    "revision": "9928b348bcc440f422fd",
+    "url": "/getDiller/static/js/2.bf178429.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/getDiller/static/js/2.bf178429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd1f3dc774611d69e39",
+    "url": "/getDiller/static/js/main.50da083f.chunk.js"
+  },
+  {
+    "revision": "01d365380b0bf1b2283a",
+    "url": "/getDiller/static/js/runtime-main.16136f77.js"
+  }
+]);
